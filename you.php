2c6c02c74cc7fa34602fedddca88050350bea8f8
bzdsl <1851060093@e.tlu.edu.vn>
2022-01-17 23:01:00 +0700

    test

--- you.php
+++ you.php
@@ -55,60 +55,92 @@ $id = $_GET['id'];
 
     <?php require_once "config/config.php"; ?>
     <div class="container">
-        <center>
-            <?php
-            $sql1 = "SELECT * FROM tbl_user where id='$id'";
-            $res1 = mysqli_query($conn,  $sql1);
-            $row12 = mysqli_fetch_assoc($res1)
-            ?>
+        <?php
+        $sql1 = "SELECT * FROM tbl_user where id='$id'";
+        $res1 = mysqli_query($conn,  $sql1);
+        $row12 = mysqli_fetch_assoc($res1)
+        ?>
 
-            <div>
-                <div>
-                    <h2>Giới thiệu</h2>
-                    <div>
-                        <div class="">Họ và Tên: <?php echo $row12['lname'];
-                                                    echo " ";
-                                                    echo $row12['fname']; ?></div>
-                    </div>
-                    <div class="">
-                        <div class="">Age: <?= $row12['age'] ?></div>
-                    </div>
-                    <div>
-                        <div class="">Email: <?= $row12['email'] ?></div>
-                    </div>
-                    <div class="">
-                        <a style="font-size: 15px;" class="btn" href="update.php?id=<?php echo $id; ?>">Thay đổi thông tin</a>
-                    </div>
+
+        <div class="container mt-3">
+            <div class="row">
+                <div class="">Full name: <?php echo $row12['lname'];
+                                            echo " ";
+                                            echo $row12['fname']; ?></div>
+                <div class="">
+                    <div class="">Age: <?= $row12['age'] ?></div>
                 </div>
+                <div>
+                    <div class="">Email: <?= $row12['email'] ?></div>
+                </div>
+                <a style="font-size: 15px;" class="link-danger" href="update.php?id=<?php echo $id; ?>">Change info</a>
 
             </div>
-        </center>
+        </div>
+        <hr>
+        <div class="container display">
+            <?php
+            $sql = "SELECT * FROM images where idkhach='$id'";
+            $res = mysqli_query($conn,  $sql);
+
+            if (mysqli_num_rows($res) > 0) {
+                while ($row = mysqli_fetch_assoc($res)) {  ?>
+
+                    <div class="alb">
+                        <img src="img/upload/<?php echo $row['image_url']; ?>">
+                        <p class="text-center mt-1"><?php echo $row["title"] ?></p>
+
+                        <form action="admin/delete-img.php?id=<?php echo $id; ?>" method="POST">
+                            <input type="hidden" name="delete_id" value="<?php echo $row['id']; ?>">
+                            <input type="hidden" name="delete_url" value="<?php echo $row['image_url']; ?>">
+                            <input type="hidden" name="delete_title" value="<?php echo $row['title']; ?>">
+                            <center> <button type="submit" name="submit" href="" class="btn btn-sm btn-danger ">Delete</button> </center>
+                        </form>
+
+                    </div>
+
+            <?php }
+            } ?>
+        </div>
     </div>
-    <div class="container display">
-        <?php
-        $sql = "SELECT * FROM images where idkhach='$id'";
-        $res = mysqli_query($conn,  $sql);
+    <!-- Footer -->
+    <footer class="bg-black text-center text-white">
+        <!-- Grid container -->
+        <div class="container p-4">
+            <!-- Section: Social media -->
+            <section class="mb-4">
+                <!-- Facebook -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
 
-        if (mysqli_num_rows($res) > 0) {
-            while ($row = mysqli_fetch_assoc($res)) {  ?>
+                <!-- Twitter -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-twitter"></i></a>
 
-                <div class="alb">
-                    <img src="img/upload/<?php echo $row['image_url']; ?>">
-                    <p class="text-center mt-1"><?php echo $row["title"] ?></p>
+                <!-- Google -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-google"></i></a>
 
-                    <form action="admin/delete-img.php?id=<?php echo $id; ?>" method="POST">
-                        <input type="hidden" name="delete_id" value="<?php echo $row['id']; ?>">
-                        <input type="hidden" name="delete_url" value="<?php echo $row['image_url']; ?>">
-                        <input type="hidden" name="delete_title" value="<?php echo $row['title']; ?>">
-                        <center> <button type="submit" name="submit" href="" class="btn btn-sm btn-danger ">Delete</button> </center>
-                    </form>
+                <!-- Instagram -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i></a>
 
-                </div>
+                <!-- Linkedin -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
 
-        <?php }
-        } ?>
-    </div>
+                <!-- Github -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-github"></i></a>
+            </section>
+            <!-- Section: Social media -->
 
+            <!-- Section: Form -->
+
+        </div>
+        <!--Grid row-->
+
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2020 Copyright:
+            <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 
